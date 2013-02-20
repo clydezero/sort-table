daadc2bb919be7b49391cb2326633ebaf01eb9b5
IvanovEAn <clydezero@aol.ru>
2013-02-20 21:42:34 +0400

    Init SortTable

---
+++ SortTable/SortTable/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SortTable
+//
+//  Created by Администратор on 20.02.13.
+//  Copyright (c) 2013 Администратор. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
